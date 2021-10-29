c880af8dd3a1645a1c3ff4ebd18911debe99d446
Andrzej Slawek <annddrzej@gmail.com>
2021-10-29 14:52:48 +0200

    Add printing results.

--- src/com/nauka/Main.java
+++ src/com/nauka/Main.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Main {
 
@@ -21,14 +22,39 @@ public class Main {
 
             printQuantities(numOfWords, numOfSentences, numOfCharacters, numOfSyllables, numOfPolysyllables);
 
-            System.out.println("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
+            String chosenAlgorithm = getUserInput();
+            System.out.println();
 
-            System.out.println(getARIScore(numOfWords, numOfSentences, numOfCharacters));
-            System.out.println(getFKScore(numOfWords, numOfSentences, numOfSyllables));
-            System.out.println(getSMOGScore(numOfSentences, numOfPolysyllables));
-            System.out.println(getCLScore(numOfWords, numOfSentences, numOfCharacters));
+            if (chosenAlgorithm != null) {
+                double ariScore = getARIScore(numOfWords, numOfSentences, numOfCharacters);
+                double fkScore = getFKScore(numOfWords, numOfSentences, numOfSyllables);
+                double smogScore = getSMOGScore(numOfSentences, numOfPolysyllables);
+                double clScore = getCLScore(numOfWords, numOfSentences, numOfCharacters);
+                int average = (getResult(ariScore) + getResult(fkScore) + getResult(smogScore) + getResult(clScore)) / 4;
+                DecimalFormat df = new DecimalFormat("#.##");
 
-            //double score = 4.71 * (numOfCharacters / numOfWords) + 0.5 * (numOfWords / numOfSentences) - 21.43;
+                switch (chosenAlgorithm) {
+                    case "all":
+                        printResult("ARI", ariScore, getResult(ariScore));
+                        printResult("FK", fkScore, getResult(fkScore));
+                        printResult("SMOG", smogScore, getResult(smogScore));
+                        printResult("CL", clScore, getResult(clScore));
+                        System.out.println();
+                        System.out.println("This text should be understood in average by " + average + "-year-olds.");
+                        break;
+                    case "ARI":
+                        printResult("ARI", ariScore, getResult(ariScore));
+                        break;
+                    case "FK":
+                        printResult("FK", fkScore, getResult(fkScore));
+                        break;
+                    case "SMOG":
+                        printResult("SMOG", smogScore, getResult(smogScore));
+                        break;
+                    case "CL":
+                        printResult("CL", clScore, getResult(clScore));
+                }
+            }
 
         } catch (NoSuchFileException e) {
             System.out.println("File not found!");
@@ -159,68 +185,97 @@ public class Main {
         return 0.0588 * l - 0.296 * s - 15.8;
     }
 
-    public static void printResult(double words, double sentences, double characters, double score) {
+    public static String getUserInput() {
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            System.out.print("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
+            input = sc.next();
+
+            if (input.matches("ARI|FK|SMOG|CL|all")) {
+                break;
+            } else if ("exit".equals(input)) {
+                break;
+            } else {
+                System.out.println("Wrong algorithm, try again or type \"exit\".");
+            }
+
+        }
+
+        return input;
+
+    }
+
+    public static int getResult(double score) {
         int roundedScore = (int) Math.ceil(score);
-        String ageRange;
+        int age;
 
         switch (roundedScore) {
             case 1:
-                ageRange = "5-6";
+                age = 6;
                 break;
             case 2:
-                ageRange = "6-7";
+                age = 7;
                 break;
             case 3:
-                ageRange = "7-9";
+                age = 9;
                 break;
             case 4:
-                ageRange = "9-10";
+                age = 10;
                 break;
             case 5:
-                ageRange = "10-11";
+                age = 11;
                 break;
             case 6:
-                ageRange = "11-12";
+                age = 12;
                 break;
             case 7:
-                ageRange = "12-13";
+                age = 13;
                 break;
             case 8:
-                ageRange = "13-14";
+                age = 14;
                 break;
             case 9:
-                ageRange = "14-15";
+                age = 15;
                 break;
             case 10:
-                ageRange = "15-16";
+                age = 16;
                 break;
             case 11:
-                ageRange = "16-17";
+                age = 17;
                 break;
             case 12:
-                ageRange = "17-18";
+                age = 18;
                 break;
             case 13:
-                ageRange = "18-24";
-                break;
             case 14:
-                ageRange = "24+";
+                age = 24;
                 break;
             default:
-                ageRange = "";
+                age = 0;
         }
 
-        DecimalFormat df1 = new DecimalFormat("#");
-        DecimalFormat df2 = new DecimalFormat("#.##");
+        return age;
+    }
 
-        String result = "Words: " + df1.format(words) + "\n" +
-                "Sentences: " + df1.format(sentences) + "\n" +
-                "Characters: " + df1.format(characters) + "\n" +
-                "The score is: " + df2.format(score) + "\n" +
-                "This text should be understood by " + ageRange + "-year-olds.";
+    public static void printResult(String chosenAlgorithm, double score, int age) {
+        DecimalFormat df = new DecimalFormat("#.##");
 
-        System.out.println(result);
-
+        switch (chosenAlgorithm) {
+            case "ARI":
+                System.out.println("Automated Readability Index: " + df.format(score) + " (about " + age + "-year-olds).");
+                break;
+            case "FK":
+                System.out.println("Flesch–Kincaid readability tests: " + df.format(score) + " (about " + age + "-year-olds).");
+                break;
+            case "SMOG":
+                System.out.println("Simple Measure of Gobbledygook: " + df.format(score) + " (about " + age + "-year-olds).");
+                break;
+            case "CL":
+                System.out.println("Coleman–Liau index: " + df.format(score) + " (about " + age + "-year-olds).");
+                break;
+        }
     }
 
 }
